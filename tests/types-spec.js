c900d9e67a8a73aa464b7215d7a6951de8c9244e
Kyle Davis <kad@fb.com>
2015-09-13 20:43:39 -0700

    update readme and tests

--- tests/types-spec.js
+++ tests/types-spec.js
@@ -36,5 +36,29 @@ function b(node: Node) {
   }
 }
 
+function getName(node: Node): string {
+  switch (node.type) {
+    case 'Identifier':
+      return node.name;
+
+    case 'ClassDeclaration':
+      // $ExpectError
+      return node.id.name; // Error, id could be null.
+
+    case 'FunctionExpression':
+      if (node.id) {
+        return node.id.name; // Can refine id to make sure it exists though.
+      }
+
+    case 'FunctionDeclaration':
+      return node.id.name; // No need to refine if it's always there.
+
+    case 'Literal':
+      // $ExpectError
+      return node.name; // Error, Literals don't have names, don't be silly.
+  }
+  return 'Unknown';
+}
+
 // Some helper functions to assert the values of different types.
 function assertString(x: string) {}
